0b409198dc2fe93113505276fe17e3c0179a7af7
Sergio Miguel Freitas Martins <sergmigu@c2r2s2.42lisboa.com>
2024-04-06 16:54:03 +0100

    send

---
+++ sergio_last/Assets/TieFighters/TieFighterLaser.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TieFighterLaser : MonoBehaviour
+{
+    public Rigidbody2D Laser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Laser.velocity = Vector2.down * 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(transform.position.y < -Camera.main.orthographicSize - 2)
+			Death();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+		if( other.gameObject.GetComponent<x_wing>().imunity_shield == false)
+        	other.gameObject.GetComponent<x_wing>().hp--;
+        Death();
+    }
+
+	private void	Death()
+	{
+		Destroy(gameObject);
+	}
+}
