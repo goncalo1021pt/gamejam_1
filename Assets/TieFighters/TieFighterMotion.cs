5661cfb58985ff6debc249690d83fa28bda7a270
Rogerio Castelo Castanheira Da Silva <rcastelo@c2r3s1.42lisboa.com>
2024-04-06 13:11:09 +0100

    Is now playable

---
+++ Assets/TieFighters/TieFighterMotion.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TieFighterMotion : MonoBehaviour
+{
+    public Rigidbody2D TieFighter;
+    public Camera Camera;
+    private float TieVelocity = 10;
+    private bool start = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TieFighter.velocity = Vector2.down * TieVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start && transform.position.y < Camera.main.orthographicSize - 2)
+        {
+            start = false;
+            TieFighter.velocity = Vector2.right * TieVelocity;
+        }
+        if (transform.position.x < -Camera.main.orthographicSize * 2 + 2)
+            TieFighter.velocity = Vector2.right * TieVelocity; 
+        if (transform.position.x > Camera.main.orthographicSize * 2 - 2)
+            TieFighter.velocity = Vector2.left * TieVelocity;
+    }
+}
